d850abc3050edc8b7343fc596347a25ad51b0019
Adrian <adriax455@gmail.com>
2026-01-05 10:53:22 -0500

    Add initial project structure with .gitkeep files and source modules

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+Sistema de Recomendación para Asignación Docente
+Carrera de Software - Universidad de Guayaquil
+
+Autores:
+- Adrian Josué Coello Araujo
+- Brayan Alejandro Hidalgo Robles
+
+Tutor:
+- PhD. Alfonso Aníbal Guijarro Rodríguez
+"""
+
+__version__ = '1.0.0'
+__authors__ = ['Adrian Coello', 'Brayan Hidalgo']
